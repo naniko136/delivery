a2299d32769c6234326ca8b873bd47798c8de11c
naniko136 <muradashvilinani1@gmail.com>
2026-03-03 19:17:39 +0400

    delivery

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Cart } from './components/cart/cart';
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'cart',
+    component: Cart,
+  },
+  {
+    path: 'log-in',
+    component: Login,
+  },
+  {
+    path: 'register',
+    component: Register,
+  },
+];
